c6448390c327b4136146ba376bb1e5fc4180b249
Deivit <david.granaods@iecisa.altia.es>
2020-07-13 13:26:20 +0200

    reparado el css y html para comenzar a trabajar.

--- asv-app/src/App.js
+++ asv-app/src/App.js
@@ -5,12 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">    
-        <p>
+      <header className="App-header">         
+        <p> A.S.V. Ambulance Service. </p>
+      </header>
+      <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        
-      </header>
     </div>
   );
 }
