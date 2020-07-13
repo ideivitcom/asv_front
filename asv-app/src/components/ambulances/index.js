dcf058fcc39a0063886915aee6f15a442d0d8696
Deivit <david.granaods@iecisa.altia.es>
2020-07-13 19:18:56 +0200

    added componenet for ambulances

--- asv-app/src/components/ambulances/index.js
+++ asv-app/src/components/ambulances/index.js
@@ -1,79 +1,76 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MaterialTable from 'material-table';
 import 'whatwg-fetch';
 
 
 function TableAmbulances() {
     const { useState } = React;
-  
+
     const [columns, setColumns] = useState([
-      { title: 'Ambulance Id', field: 'id' },
-      { title: 'Max Capacity', field: 'seats' },
-     
+        { title: 'Ambulance Id', field: 'id' },
+        { title: 'Max Capacity', field: 'seats' },
+
     ]);
 
     const URL_GET_AMBULANCES = 'http://localhost:5000/api/ambulances'
 
-    const [data, setData] = useState([
-      
-    ]);
-  
-    return (
-      <MaterialTable
-        title="Ambulances"
-        columns={columns}
-        data={data}
-        options={{
-          search:false
-        }}
-        data={query =>
-          new Promise((resolve, reject) => {
-            let url = URL_GET_AMBULANCES           
-            fetch(url)
-              .then(response => response.json())
-              .then(result => {
-                resolve({
-                  data: result,                 
-                  totalCount: result.total,
-                })
-              })
-          })
-        }
-        editable={{
-          onRowAdd: newData =>
-            new Promise((resolve, reject) => {
-              setTimeout(() => {
-                setData([...data, newData]);
-                
-                resolve();
-              }, 1000)
-            }),
-          onRowUpdate: (newData, oldData) =>
-            new Promise((resolve, reject) => {
-              setTimeout(() => {
-                const dataUpdate = [...data];
-                const index = oldData.tableData.id;
-                dataUpdate[index] = newData;
-                setData([...dataUpdate]);
-  
-                resolve();
-              }, 1000)
-            }),
-          onRowDelete: oldData =>
-            new Promise((resolve, reject) => {
-              setTimeout(() => {
-                const dataDelete = [...data];
-                const index = oldData.tableData.id;
-                dataDelete.splice(index, 1);
-                setData([...dataDelete]);
-                
-                resolve()
-              }, 1000)
-            }),
-        }}
-      />
-    )
-  }
+    const [data, setData] = useState([]);
+    const [dirty, setDirty] = useState(false)
 
-  export default TableAmbulances;
-  
+    useEffect(() => {
+        fetch(URL_GET_AMBULANCES)
+            .then(res => res.json())
+            .then(json => setData(json));
+
+    }, [])
+
+    return (
+        <MaterialTable
+            title="Ambulances"
+            columns={columns}
+            data={data}
+            options={{
+                paging: false,
+                search: false
+            }}
+            data={
+                data
+            }
+            editable={{
+                onRowAdd: newData =>
+                    new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            setData([...data, newData]);
+
+                            resolve();
+                        }, 1000)
+                    }),
+
+                onRowUpdate: (newData, oldData) =>
+                    new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            const dataUpdate = [...data];
+                            const index = oldData.tableData.id;
+                            dataUpdate[index] = newData;
+                            setData([...dataUpdate]);
+
+                            resolve();
+                        }, 1000)
+                    }),
+                onRowDelete: oldData =>
+                    new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            const dataDelete = [...data];
+                            const index = oldData.tableData.id;
+                            dataDelete.splice(index, 1);
+                            setData([...dataDelete]);
+
+                            resolve()
+                        }, 1000)
+                    }),
+            }}
+        />
+    )
+}
+
+export default TableAmbulances;
