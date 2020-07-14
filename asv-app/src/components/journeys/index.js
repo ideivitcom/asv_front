c7d3b0aa75f42f3cc2349752715a396b22ef7b63
Deivit <david.granaods@iecisa.altia.es>
2020-07-14 13:16:08 +0200

    journeys tiene ahora notificaciones

--- asv-app/src/components/journeys/index.js
+++ asv-app/src/components/journeys/index.js
@@ -2,7 +2,12 @@ import React, { useEffect } from "react";
 import MaterialTable, { MTableToolbar } from 'material-table';
 import 'whatwg-fetch';
 import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
 
 
 function TableJourneys() {
@@ -18,7 +23,9 @@ function TableJourneys() {
     const URL_PUT_JOURNEYS = 'http://127.0.0.1:5000/api/journeys/'
 
     const [data, setData] = useState([]);
-    
+    const [open, setOpen] = React.useState(false);
+    const [message, setMessage] = useState('')
+    const [state, setState] = useState('info')
 
     useEffect(() => {
         fetch(URL_GET_JOURNEYS)
@@ -27,91 +34,132 @@ function TableJourneys() {
 
     }, [])
 
-     
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+
+
     return (
-        <MaterialTable
-            title="Journeys"
-            columns={columns}
-            data={data}
-            options={{
-                paging: false,
-                search: false
-            }}
-            data={
-                data
-            }
-            components={{
-                Toolbar: props => (
-                    <div>
-                        <MTableToolbar {...props} />
-                        <div style={{ padding: '0px 10px' }}>
-                            <Button variant="contained" color="primary"onClick={() =>
-                            {                            
+        <div>
+            <MaterialTable
+                title="Journeys"
+                columns={columns}
+                data={data}
+                options={{
+                    paging: false,
+                    search: false
+                }}
+                data={
+                    data
+                }
+                components={{
+                    Toolbar: props => (
+                        <div>
+                            <MTableToolbar {...props} />
+                            <div style={{ padding: '0px 10px' }}>
+                                <Button variant="contained" color="primary" onClick={() => {
 
-                                fetch(URL_PUT_JOURNEYS, {                                    
-                                  
-                                    method:'PUT',
-                                    headers: {
-                                    'Accept': 'application/json',
-                                    'Content-Type': 'application/json'                                    
-                                    },
-                                    body: JSON.stringify(data)
-                                })
-                                     
-                                                               
-                                      
-                             }}> Save </Button>
-                            <Button variant="contained" color="Secondary"onClick={() => {
-                                fetch(URL_GET_JOURNEYS,{
-                                    method:'GET',
-                                    headers: {
-                                    'Accept': 'application/json',
-                                    'Content-Type': 'application/json'                                    
-                                    },                                   
-                                })
-                                .then(res => res.json())
-                                .then(json => setData(json));
+                                    fetch(URL_PUT_JOURNEYS, {
 
-                            }}
-                              > Restore </Button>
+                                        method: 'PUT',
+                                        headers: {
+                                            'Accept': 'application/json',
+                                            'Content-Type': 'application/json'
+                                        },
+                                        body: JSON.stringify(data)
+                                    }).then(
+                                        function(response){
+                                           setMessage("Jorneys was saved")
+                                           setState('success')
+                                           setOpen(true)
+                                        }
+                                      )
+                                        .catch(
+                                            function(response){
+                                                setMessage("There was a problem saving the data")
+                                                setState('error')
+                                                setOpen(true)
+                                             }
+                                        );
+
+
+
+
+                                }}> Save </Button>
+                                <Button variant="contained" color="Secondary" onClick={() => {
+                                    fetch(URL_GET_JOURNEYS, {
+                                        method: 'GET',
+                                        headers: {
+                                            'Accept': 'application/json',
+                                            'Content-Type': 'application/json'
+                                        },
+                                    })
+                                    .then(res => res.json())
+                                    .then(function(json){
+                                        setData(json)
+                                        setMessage("The changes was discarted")
+                                        setState('success')
+                                        setOpen(true)
+                                     })
+                                    .catch(
+                                        function(response){
+                                            setMessage("There was a problem saving the data")
+                                            setState('error')
+                                            setOpen(true)
+                                         }
+                                    );
+
+                                }}
+                                > Restore </Button>
+                            </div>
                         </div>
-                    </div>
-                ),
-            }}
-            editable={{
-                onRowAdd: newData =>
-                    new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                            setData([...data, newData]);
+                    ),
+                }}
+                editable={{
+                    onRowAdd: newData =>
+                        new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                setData([...data, newData]);
 
-                            resolve();
-                        }, 1000)
-                    }),
+                                resolve();
+                            }, 1000)
+                        }),
 
-                onRowUpdate: (newData, oldData) =>
-                    new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                            const dataUpdate = [...data];
-                            const index = oldData.tableData.id;
-                            dataUpdate[index] = newData;
-                            setData([...dataUpdate]);
+                    onRowUpdate: (newData, oldData) =>
+                        new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                const dataUpdate = [...data];
+                                const index = oldData.tableData.id;
+                                dataUpdate[index] = newData;
+                                setData([...dataUpdate]);
 
-                            resolve();
-                        }, 1000)
-                    }),
-                onRowDelete: oldData =>
-                    new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                            const dataDelete = [...data];
-                            const index = oldData.tableData.id;
-                            dataDelete.splice(index, 1);
-                            setData([...dataDelete]);
+                                resolve();
+                            }, 1000)
+                        }),
+                    onRowDelete: oldData =>
+                        new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                const dataDelete = [...data];
+                                const index = oldData.tableData.id;
+                                dataDelete.splice(index, 1);
+                                setData([...dataDelete]);
 
-                            resolve()
-                        }, 1000)
-                    }),
-            }}
-        />
+                                resolve()
+                            }, 1000)
+                        }),
+                }}
+            />
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity={state}>
+                    {message}
+                </Alert>
+            </Snackbar>
+        </div>
     )
 }
 
